241289cc4d20673f3c91aa23b123157c5cf81602
AdamRB208 <adamrborup@gmail.com>
2025-12-11 15:59:54 -0700

    set up variable data

--- main/main.py
+++ main/main.py
@@ -3,8 +3,33 @@ score = 0
 
 questions = [
   {
-    'question': "...",
-    'options' : ['A)...', 'B)...', 'C)...', 'D)...'],
+    'question': "What is the largest Planet in the Solar System?",
+    'options' : ['A) The Sun', 'B) Jupiter', 'C) Saturn', 'D) Earth'],
+    'answer' : 'B'
+  },
+  {
+    'question': "What piece of Venoms body can turn into a new symbiote?",
+    'options' : ['A) His fingers', 'B) His eyes', 'C) His tongue', 'D) Any part of him'],
+    'answer' : 'C'
+  },
+  {
+    'question': "In StarWars C-3PO is fluent in how many forms of communication?",
+    'options' : ['A) over 3 Million', 'B) 3 Thousand', 'C) 8 Thousand', 'D) over 6 Million'],
+    'answer' : 'D'
+  },
+  {
+    'question': "In StarWars what is the name of Poe Dameron's droid?",
+    'options' : ['A) BB-8', 'B) C-3PO', 'C) R2 D2', 'D) BD-8'],
     'answer' : 'A'
+  },
+  {
+    'question': "What does Tony Stark call his AI Assistant?",
+    'options' : ['A) Casper', 'B) Janis', 'C) Gleebour', 'D) Jarvis'],
+    'answer' : 'D'
+  },
+  {
+    'question': "What planet has the largest volcano in the solar system?",
+    'options' : ['A) Earth', 'B) Mars', 'C) Venus', 'D) Pluto'],
+    'answer' : 'B'
   }
 ]
